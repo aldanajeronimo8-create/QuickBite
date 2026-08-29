873dc113d2bad3ad903b9137d62d642c69d8fb95
aldanajeronimo8-create <aldanajeronimo8@gmail.com>
2026-08-29 14:19:38 -0500

    feat(student): mount feature hub on student menu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useLocation } from 'react-router-dom';
 import { router } from './routes';
 import { Toaster } from './components/ui/sonner';
 import { useAuthStore } from '../store/authStore';
@@ -8,6 +8,23 @@ import { ErrorBoundary } from './components/system/ErrorBoundary';
 import { hasSupabaseConfig, needsFirstRunSetup } from '../config/appConfig';
 import { SetupWizardPage } from './pages/SetupWizardPage';
 import { supabase } from '../lib/supabase';
+import { StudentFeatureHub } from './components/student/StudentFeatureHub';
+
+function StudentEnhancements() {
+  const location = useLocation();
+  const user = useAuthStore((state) => state.user);
+  if (location.pathname !== '/menu' || !user) return null;
+  return <StudentFeatureHub userId={user.id} />;
+}
+
+function AppShell() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <StudentEnhancements />
+    </>
+  );
+}
 
 function App() {
   const checkSession = useAuthStore((s) => s.checkSession);
@@ -25,12 +42,9 @@ function App() {
     let cleanupRealtime = subscribeRealtime();
     const { data } = supabaseClient.auth.onAuthStateChange((_event, session) => {
       cleanupRealtime();
-      if (session?.access_token) {
-        supabaseClient.realtime.setAuth(session.access_token);
-      }
+      if (session?.access_token) supabaseClient.realtime.setAuth(session.access_token);
       cleanupRealtime = subscribeRealtime();
     });
-
     return () => {
       cleanupRealtime();
       data.subscription?.unsubscribe();
@@ -40,7 +54,7 @@ function App() {
   return (
     <>
       <ErrorBoundary>
-        {needsSetup ? <SetupWizardPage /> : <RouterProvider router={router} />}
+        {needsSetup ? <SetupWizardPage /> : <AppShell />}
       </ErrorBoundary>
       <Toaster position="top-center" />
     </>
