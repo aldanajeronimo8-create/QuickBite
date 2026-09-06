38d59be200284686aefba526d0d722ce729b1ee9
aldanajeronimo8-create <aldanajeronimo8@gmail.com>
2026-09-06 14:39:33 -0500

    feat(theme): enable development dark-surface audit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './app/App';
+import { startThemeAudit } from './lib/themeAuditBootstrap';
+
+const stopThemeAudit = startThemeAudit();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => stopThemeAudit());
+}
