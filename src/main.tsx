e93faefdb8f6ac732c4186c0f780a975ff315364
aldanajeronimo8-create <aldanajeronimo8@gmail.com>
2026-09-10 09:45:41 -0500

    fix(admin): load dedicated dark mode harmony layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
+import './styles/admin-dark.css';
 import App from './app/App';
 import { startThemeAudit } from './lib/themeAuditBootstrap';
 
