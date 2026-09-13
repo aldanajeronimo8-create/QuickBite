777046f686e71261bbab00c6168043732104f88a
aldanajeronimo8-create <aldanajeronimo8@gmail.com>
2026-09-13 17:16:02 -0500

    feat: load Phase 0 admin harmony layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import './styles/admin-dark.css';
+import './styles/phase0-admin-harmony.css';
 import App from './app/App';
 import { startThemeAudit } from './lib/themeAuditBootstrap';
 
