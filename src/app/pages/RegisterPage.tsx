881d3e5d93f61ebd6ee402afeac3642940db6068
aldanajeronimo8-create <aldanajeronimo8@gmail.com>
2026-09-13 17:02:45 -0500

    fix: restore register icon class

--- src/app/pages/RegisterPage.tsx
+++ src/app/pages/RegisterPage.tsx
@@ -301,7 +301,7 @@ export function RegisterPage() {
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-blue-300 hover:text-blue-200 transition"
                 >
-                  {showConfirmPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 w-5" />}
+                  {showConfirmPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
                 </button>
               </div>
               {formData.confirmPassword && formData.password === formData.confirmPassword && (
